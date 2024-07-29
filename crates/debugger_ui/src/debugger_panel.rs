720a4c348b6b175b4b44189b926dd97403ab88e5
Remco Smits <djsmits12@gmail.com>
2024-07-29 19:37:54 +0200

    Log error instead of ignoring it

--- crates/debugger_ui/src/debugger_panel.rs
+++ crates/debugger_ui/src/debugger_panel.rs
@@ -467,15 +467,22 @@ impl DebugPanel {
                     if !existing_item {
                         let debug_panel = cx.view().clone();
 
-                        let _ = this.workspace.update(cx, |_, cx| {
-                            this.pane.update(cx, |this, cx| {
-                                let tab = cx.new_view(|cx| {
-                                    DebugPanelItem::new(debug_panel, client.clone(), thread_id, cx)
-                                });
+                        this.workspace
+                            .update(cx, |_, cx| {
+                                this.pane.update(cx, |this, cx| {
+                                    let tab = cx.new_view(|cx| {
+                                        DebugPanelItem::new(
+                                            debug_panel,
+                                            client.clone(),
+                                            thread_id,
+                                            cx,
+                                        )
+                                    });
 
-                                this.add_item(Box::new(tab.clone()), false, false, None, cx)
+                                    this.add_item(Box::new(tab.clone()), false, false, None, cx)
+                                })
                             })
-                        });
+                            .log_err();
                     }
 
                     cx.emit(DebugPanelEvent::Stopped((client_id, event)));
