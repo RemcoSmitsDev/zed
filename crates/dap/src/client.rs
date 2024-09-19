eae8a160ab7bbec7b1ae1f8d99bf14935db8bf74
Remco Smits <djsmits12@gmail.com>
2024-09-19 20:35:02 +0200

    Remove step back
    we don't support this yet

--- crates/dap/src/client.rs
+++ crates/dap/src/client.rs
@@ -4,10 +4,9 @@ use anyhow::{anyhow, Context, Result};
 use crate::adapters::{build_adapter, DebugAdapter};
 use dap_types::{
     messages::{Message, Response},
-    requests::{Disconnect, Request, SetBreakpoints, StepBack, Terminate, Variables},
+    requests::{Disconnect, Request, SetBreakpoints, Terminate, Variables},
     DisconnectArguments, Scope, SetBreakpointsArguments, SetBreakpointsResponse, Source,
-    SourceBreakpoint, StackFrame, StepBackArguments, SteppingGranularity, TerminateArguments,
-    Variable, VariablesArguments,
+    SourceBreakpoint, StackFrame, TerminateArguments, Variable, VariablesArguments,
 };
 use futures::{AsyncBufRead, AsyncWrite};
 use gpui::{AppContext, AsyncAppContext};
@@ -230,25 +229,6 @@ impl DebugAdapterClient {
     /// Get the next sequence id to be used in a request
     pub fn next_sequence_id(&self) -> u64 {
         self.sequence_count.fetch_add(1, Ordering::Relaxed)
-    }
-
-    pub async fn step_back(&self, thread_id: u64, granularity: SteppingGranularity) -> Result<()> {
-        // TODO debugger: make this work again
-        let supports_single_thread_execution_requests = true;
-        // let supports_single_thread_execution_requests = capabilities
-        //     .supports_single_thread_execution_requests
-        //     .unwrap_or_default();
-        let supports_stepping_granularity = true;
-        // let supports_stepping_granularity = capabilities
-        //     .supports_stepping_granularity
-        //     .unwrap_or_default();
-
-        self.request::<StepBack>(StepBackArguments {
-            thread_id,
-            granularity: supports_stepping_granularity.then(|| granularity),
-            single_thread: supports_single_thread_execution_requests.then(|| true),
-        })
-        .await
     }
 
     pub async fn set_breakpoints(
