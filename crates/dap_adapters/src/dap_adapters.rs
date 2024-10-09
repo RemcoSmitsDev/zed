07e4fec1515e7209645179ec3de35987b63361fb
Remco Smits <djsmits12@gmail.com>
2024-10-09 15:01:02 +0200

    Clean up how adapters install and fetch their binary
    Before this was in one method, which was hard to read and doing to many things.

--- crates/dap_adapters/src/dap_adapters.rs
+++ crates/dap_adapters/src/dap_adapters.rs
@@ -30,12 +30,11 @@ use task::{CustomArgs, DebugAdapterConfig, DebugAdapterKind, DebugConnectionType
 
 pub fn build_adapter(adapter_config: &DebugAdapterConfig) -> Result<Box<dyn DebugAdapter>> {
     match &adapter_config.kind {
-        DebugAdapterKind::Custom(start_args) => Ok(Box::new(CustomDebugAdapter::new(
-            adapter_config,
-            start_args.clone(),
-        ))),
-        DebugAdapterKind::Python => Ok(Box::new(PythonDebugAdapter::new(adapter_config))),
-        DebugAdapterKind::PHP => Ok(Box::new(PhpDebugAdapter::new(adapter_config))),
-        DebugAdapterKind::Lldb => Ok(Box::new(LldbDebugAdapter::new(adapter_config))),
+        DebugAdapterKind::Custom(start_args) => {
+            Ok(Box::new(CustomDebugAdapter::new(start_args.clone())))
+        }
+        DebugAdapterKind::Python => Ok(Box::new(PythonDebugAdapter::new())),
+        DebugAdapterKind::PHP => Ok(Box::new(PhpDebugAdapter::new())),
+        DebugAdapterKind::Lldb => Ok(Box::new(LldbDebugAdapter::new())),
     }
 }
