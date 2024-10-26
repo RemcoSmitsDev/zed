a2de63c9b60b006392a63e98b28199f40f0a71dc
Remco Smits <djsmits12@gmail.com>
2024-10-26 16:25:07 +0200

    Don't send SelectedStackFrameChanged when id is still the same

--- crates/debugger_ui/src/stack_frame_list.rs
+++ crates/debugger_ui/src/stack_frame_list.rs
@@ -109,9 +109,13 @@ impl StackFrameList {
             let task = this.update(&mut cx, |this, cx| {
                 std::mem::swap(&mut this.stack_frames, &mut stack_frames);
 
+                let previous_stack_frame_id = this.current_stack_frame_id;
                 if let Some(stack_frame) = this.stack_frames.first() {
                     this.current_stack_frame_id = stack_frame.id;
-                    cx.emit(StackFrameListEvent::SelectedStackFrameChanged);
+
+                    if previous_stack_frame_id != this.current_stack_frame_id {
+                        cx.emit(StackFrameListEvent::SelectedStackFrameChanged);
+                    }
                 }
 
                 this.list.reset(this.stack_frames.len());
