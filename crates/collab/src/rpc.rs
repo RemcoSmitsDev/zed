fcab4fae29286a469906139b5c20a2d69599f106
Anthony Eid <hello@anthonyeid.me>
2025-02-04 10:32:25 -0500

    Remove debugger tables from collab db

--- crates/collab/src/rpc.rs
+++ crates/collab/src/rpc.rs
@@ -424,10 +424,12 @@ impl Server {
             .add_message_handler(
                 broadcast_project_message_from_host::<proto::RemoveActiveDebugLine>,
             )
-            .add_message_handler(set_debug_client_panel_item)
-            .add_message_handler(update_debug_adapter)
-            .add_message_handler(update_debug_client_capabilities)
-            .add_message_handler(shutdown_debug_client)
+            .add_message_handler(broadcast_project_message_from_host::<proto::SetDebuggerPanelItem>)
+            .add_message_handler(broadcast_project_message_from_host::<proto::UpdateDebugAdapter>)
+            .add_message_handler(
+                broadcast_project_message_from_host::<proto::SetDebugClientCapabilities>,
+            )
+            .add_message_handler(broadcast_project_message_from_host::<proto::ShutdownDebugClient>)
             .add_request_handler(forward_mutating_project_request::<proto::DapNextRequest>)
             .add_request_handler(forward_mutating_project_request::<proto::DapStepInRequest>)
             .add_request_handler(forward_mutating_project_request::<proto::DapStepOutRequest>)
@@ -449,7 +451,9 @@ impl Server {
             .add_message_handler(
                 broadcast_project_message_from_host::<proto::ToggleIgnoreBreakpoints>,
             )
-            .add_message_handler(ignore_breakpoint_state)
+            .add_message_handler(
+                broadcast_project_message_from_host::<proto::IgnoreBreakpointState>,
+            )
             .add_message_handler(broadcast_project_message_from_host::<proto::DebuggerSessionEnded>)
             .add_request_handler(
                 forward_mutating_project_request::<proto::ActiveDebugSessionsRequest>,
@@ -2137,117 +2141,6 @@ async fn update_language_server(
     broadcast(
         Some(session.connection_id),
         project_connection_ids.iter().copied(),
-        |connection_id| {
-            session
-                .peer
-                .forward_send(session.connection_id, connection_id, request.clone())
-        },
-    );
-    Ok(())
-}
-
-/// Notify other participants that a debug client has shutdown
-async fn shutdown_debug_client(
-    request: proto::ShutdownDebugClient,
-    session: Session,
-) -> Result<()> {
-    let guest_connection_ids = session
-        .db()
-        .await
-        .shutdown_debug_client(session.connection_id, &request)
-        .await?;
-
-    broadcast(
-        Some(session.connection_id),
-        guest_connection_ids.iter().copied(),
-        |connection_id| {
-            session
-                .peer
-                .forward_send(session.connection_id, connection_id, request.clone())
-        },
-    );
-    Ok(())
-}
-
-async fn ignore_breakpoint_state(
-    request: proto::IgnoreBreakpointState,
-    session: Session,
-) -> Result<()> {
-    let guest_connection_ids = session
-        .db()
-        .await
-        .ignore_breakpoint_state(session.connection_id, &request)
-        .await?;
-
-    broadcast(
-        Some(session.connection_id),
-        guest_connection_ids.iter().copied(),
-        |connection_id| {
-            session
-                .peer
-                .forward_send(session.connection_id, connection_id, request.clone())
-        },
-    );
-    Ok(())
-}
-
-/// Notify other participants that a debug panel item has been updated
-async fn update_debug_adapter(request: proto::UpdateDebugAdapter, session: Session) -> Result<()> {
-    let guest_connection_ids = session
-        .db()
-        .await
-        .update_debug_adapter(session.connection_id, &request)
-        .await?;
-
-    broadcast(
-        Some(session.connection_id),
-        guest_connection_ids.iter().copied(),
-        |connection_id| {
-            session
-                .peer
-                .forward_send(session.connection_id, connection_id, request.clone())
-        },
-    );
-    Ok(())
-}
-
-/// Notify other participants that there's a new debug panel item
-async fn set_debug_client_panel_item(
-    request: proto::SetDebuggerPanelItem,
-    session: Session,
-) -> Result<()> {
-    let guest_connection_ids = session
-        .db()
-        .await
-        .set_debug_client_panel_item(session.connection_id, &request)
-        .await?;
-
-    broadcast(
-        Some(session.connection_id),
-        guest_connection_ids.iter().copied(),
-        |connection_id| {
-            session
-                .peer
-                .forward_send(session.connection_id, connection_id, request.clone())
-        },
-    );
-    Ok(())
-}
-
-/// Notify other participants that a debug client's capabilities has been created or updated
-async fn update_debug_client_capabilities(
-    request: proto::SetDebugClientCapabilities,
-    session: Session,
-) -> Result<()> {
-    let guest_connection_ids = session
-        .db()
-        .await
-        .update_debug_client_capabilities(session.connection_id, &request)
-        .await?;
-
-    broadcast(
-        Some(session.connection_id),
-        guest_connection_ids.iter().copied(),
         |connection_id| {
             session
                 .peer
